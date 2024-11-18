94df604e71cd40468c054bdf14d59ccf459f0cc0
Cheongjo <103722439+cj5311@users.noreply.github.com>
2024-11-18 12:38:58 +0900

    kotlin version update

--- app/src/main/java/com/example/todolist/AddTodoActivity.kt
+++ app/src/main/java/com/example/todolist/AddTodoActivity.kt
@@ -2,6 +2,7 @@ package com.example.todolist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import com.example.todolist.databinding.ActivityAddTodoBinding
 import com.example.todolist.db.AppDatabase
@@ -24,20 +25,24 @@ class AddTodoActivity : AppCompatActivity() {
 
         //싱글톤으로 데이터베이스 생성했기 때문에, 메인엑티비티에서 호출한 객체와 동일한 객체이다.
         db = AppDatabase.getInstance(this)!!
-
-
         todoDao = db.getTodoDao()
 
+        //버튼클릭했을때 인서트 함수 실행
         binding.btnComplete.setOnClickListener {
             insertTodo()
         }
     }
 
+
     private fun insertTodo() {
+
+        //텍스트뷰의 텍스트와, 라디오그룹의 각 버튼아이디 호출
         val todoTitle = binding.edtTitle.text.toString()
         var todoImportance = binding.radioGroup.checkedRadioButtonId
 
+        Log.v("todoImportance","$todoImportance")
         var impData = 0;
+
         when(todoImportance){
             R.id.btn_high -> {
                 impData = 1;
